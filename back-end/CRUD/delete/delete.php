23e09bf720a218a120e2a6e1eedc1cb31593702c
salaahl <67805036+salaahl@users.noreply.github.com>
2022-12-13 13:24:46 +0100

    Update delete.php
    Ajout du code de suppression d'un partenaire/structure (côté back)

--- back-end/CRUD/delete/delete.php
+++ back-end/CRUD/delete/delete.php
@@ -1,1 +1,27 @@
 <?php
+
+<?php
+
+if (isset($_POST['delete_partner'])) {
+  $mail = mysqli_real_escape_string($db, $_POST['mail_partenaire']);
+
+  $partnerD = $db->prepare(
+    "DELETE FROM FitnessP_Partenaire
+    WHERE mail = ?"
+  );
+
+  $partnerD->bind_param("s", $mail);
+  $partnerD->execute();
+}
+
+if (isset($_POST['delete_structure'])) {
+  $mail = mysqli_real_escape_string($db, $_POST['mail_partenaire']);
+
+  $structureD = $db->prepare(
+    "DELETE FROM FitnessP_Structure
+    WHERE mail = ?"
+  );
+
+  $structureD->bind_param("s", $mail);
+  $structureD->execute();
+}

d77ccf9a3c3b17881686bdd8bcc1316603eba621
salaahl <67805036+salaahl@users.noreply.github.com>
2023-02-22 18:20:25 +0100

    Update changer_mdp.js
    Modifs mineures

--- scripts/ajax/changer_mdp.js
+++ scripts/ajax/changer_mdp.js
@@ -1,5 +1,5 @@
 $(function() {
-  $("#form-dish").validate({
+  $("form").validate({
     rules: {
       changer_mot_de_passe: {
         required: true,
@@ -28,26 +28,30 @@ $(function() {
     },
     submitHandler: function() {
       // Peut-être faudra-il mettre cette partie dans le 'form.submit()'
-      let searchParams = new URLSearchParams(window.location.search);
-      let mail = searchParams.get("mail");
-      let nouveauMdp = $("#changer_mot_de_passe").val();
+      if (commonPassword('#changer_mot_de_passe', '#niveau_mot_de_passe')) {
+        let searchParams = new URLSearchParams(window.location.search);
+        let mail = searchParams.get("mail");
+        let nouveauMdp = $("#changer_mot_de_passe").val();
 
-      $.ajax({
-        type: "post",
-        url: "index.php",
-        data: { mail: mail, nouveau_mdp: nouveauMdp },
-        success: function(data) {
-          if (data == "") {
-            alert("Nouveau mot de passe enregistré. Veuillez vous reconnecter.");
-            location.replace("./login.html");
-          } else {
-            alert(data);
-          }
-        },
-        error: function() {
-          alert("Erreur");
-        },
-      });
+        $.ajax({
+          type: "post",
+          url: "index.php",
+          data: { mail: mail, nouveau_mdp: nouveauMdp },
+          success: function(data) {
+            if (data == "") {
+              alert("Nouveau mot de passe enregistré. Veuillez vous reconnecter.");
+              location.replace("./login.html");
+            } else {
+              alert(data);
+            }
+          },
+          error: function() {
+            alert("Erreur");
+          },
+        });
+      } else {
+        alert("Le mot de passe est faible. Veuillez le changer.");
+      }
     }
   });
 });

8d01c32835703101041db4414efbb83af45e9a00
salaahl <67805036+salaahl@users.noreply.github.com>
2023-02-23 18:24:55 +0100

    Update and rename changer_mdp.php to change_password.php
    Refactoring du code

--- change_password.php
+++ change_password.php
@@ -22,9 +22,8 @@ if (isset($_SESSION['mail_p'])) {
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width">
   <title>Changer de mot de passe</title>
-  <link href="./css/changer_mdp.css" rel="stylesheet" type="text/css" />
+  <link href="./css/change_password.css" rel="stylesheet" type="text/css" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </head>
 
 <body>
@@ -36,9 +35,8 @@ if (isset($_SESSION['mail_p'])) {
       </h2>
 
       <div class="inputDiv">
-        <label class="inputLabel" for="changer_mot_de_passe">Nouveau mot de passe :</label>
-        <label id="niveau_mot_de_passe" name="niveau_mot_de_passe" style="color: red"></label>
-        <input type="password" id="changer_mot_de_passe" name="changer_mot_de_passe" required>
+        <label class="inputLabel" for="change_password">Nouveau mot de passe :</label>
+        <input type="password" id="change_password" name="change_password" required>
       </div>
 
       <div class="inputDiv">
@@ -49,8 +47,15 @@ if (isset($_SESSION['mail_p'])) {
       <button type="submit" class="btn btn-dark">Valider</button>
     </form>
   </div>
-  <script src="./scripts/composants/nbp.min.js"></script>
-  <script src="./scripts/ajax/changer_mdp.js"></script>
+  <script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM="
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.min.js"
+    integrity="sha512-rstIgDs0xPgmG6RX1Aba4KV5cWJbAMcvRCVmglpam9SoHZiUCyQVDdH2LPlxoHtrv17XWblE/V/PP+Tr04hbtA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/additional-methods.min.js"
+    integrity="sha512-6S5LYNn3ZJCIm0f9L6BCerqFlQ4f5MwNKq+EthDXabtaJvg3TuFLhpno9pcm+5Ynm6jdA9xfpQoMz2fcjVMk9g=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="./scripts/ajax/change_password.js"></script>
 </body>
 
 </html>

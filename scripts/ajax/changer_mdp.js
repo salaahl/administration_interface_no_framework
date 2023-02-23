469857cbc48499a5d75cbde729f680b961de695f
salaahl <67805036+salaahl@users.noreply.github.com>
2023-02-23 18:16:59 +0100

    Update changer_mdp.js
    Réécriture du script

--- scripts/ajax/changer_mdp.js
+++ scripts/ajax/changer_mdp.js
@@ -1,60 +1,62 @@
 $(function() {
+
+  $.getScript("../composants/common_password.js");
+
+  $.validator.addMethod(
+    "noCommonPassword",
+    function() {
+      return commonPassword('#change_password');
+    },
+    "Votre mot de passe n'est pas assez sécurisé. Veuillez le modifier."
+  );
+
   $("form").validate({
     rules: {
-      changer_mot_de_passe: {
+      change_password: {
         required: true,
-        minlength: 5
+        minlength: 5,
+        noCommonPassword: true
       },
-      confirmer_mot_de_passe: {
+      confirm_password: {
         required: true,
-        equalTo: "#changer_mot_de_passe"
-      },
-      niveau_mot_de_passe: {
-        equalTo: "Ok"
+        equalTo: "#change_password"
       }
     },
     messages: {
-      changer_mot_de_passe: {
-        required: "Veuillez remplir ce champ",
-        minlength: "Le mot de passe doit faire plus de cinq caractères"
+      change_password: {
+        required: "Ce champ est obligatoire.",
+        minlength: "Veuillez saisir au moins 5 caractères."
       },
-      confirmer_mot_de_passe: {
-        required: "Veuillez remplir ce champ",
-        equalTo: "Le mot de passe ne correspond pas"
-      },
-      niveau_mot_de_passe: {
-        equalTo: "Votre mot de passe est faible. Veuillez le changer"
+      confirm_password: {
+        required: "Ce champ est obligatoire.",
+        equalTo: "Veuillez entrer à nouveau la même valeur."
       }
     },
     submitHandler: function() {
-      // Peut-être faudra-il mettre cette partie dans le 'form.submit()'
-      $("form").on("submit", function (e) {
+      $("form").on("submit", function(e) {
         e.preventDefault();
-        if (commonPassword('#changer_mot_de_passe', '#niveau_mot_de_passe')) {
-          let searchParams = new URLSearchParams(window.location.search);
-          let mail = searchParams.get("mail");
-          let nouveauMdp = $("#changer_mot_de_passe").val();
+        
+        let searchParams = new URLSearchParams(window.location.search);
+        let mail = searchParams.get("mail");
+        let nouveauMdp = $("#change_password").val();
 
-          $.ajax({
-            type: "post",
-            url: "index.php",
-            data: { mail: mail, nouveau_mdp: nouveauMdp },
-            success: function(data) {
-              if (data == "") {
-                alert("Nouveau mot de passe enregistré. Veuillez vous reconnecter.");
-                location.replace("./login.html");
-              } else {
-                alert(data);
-              }
-            },
-            error: function() {
-              alert("Erreur");
-            },
-          });
-        } else {
-          alert("Le mot de passe est faible. Veuillez le changer.");
-        }
-      }
+        $.ajax({
+          type: "post",
+          url: "index.php",
+          data: { mail: mail, nouveau_mdp: nouveauMdp },
+          success: function(data) {
+            if (data == "") {
+              alert("Nouveau mot de passe enregistré. Veuillez vous reconnecter.");
+              location.replace("./login.html");
+            } else {
+              alert(data);
+            }
+          },
+          error: function() {
+            alert("Erreur. Veuillez contacter un administrateur.");
+          },
+        });
+      })
     }
-  });
+  })
 });

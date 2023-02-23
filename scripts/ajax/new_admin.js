880f7d09a973196f3329de936c6de6bacd3a3ece
salaahl <67805036+salaahl@users.noreply.github.com>
2023-02-23 19:01:52 +0100

    Update and rename nv_admin.js to new_admin.js

---
+++ scripts/ajax/new_admin.js
@@ -0,0 +1,64 @@
+$(function() {
+
+  $.getScript("../composants/common_password.js");
+
+  $.validator.addMethod(
+    "noCommonPassword",
+    function() {
+      return commonPassword('#change_password');
+    },
+    "Votre mot de passe n'est pas assez sécurisé. Veuillez le modifier."
+  );
+
+  $("form").validate({
+    rules: {
+      admin_mail: {
+        required: true
+      },
+      admin_password: {
+        required: true,
+        minlength: 5,
+        noCommonPassword: true
+      },
+      confirm_password: {
+        required: true,
+        equalTo: "#admin_password"
+      }
+    },
+    messages: {
+      admin_mail: {
+        required: "Ce champ est obligatoire."
+      },
+      admin_password: {
+        required: "Ce champ est obligatoire.",
+        minlength: "Veuillez saisir au moins 5 caractères."
+      },
+      confirm_password: {
+        required: "Ce champ est obligatoire.",
+        equalTo: "Veuillez entrer à nouveau la même valeur."
+      }
+    },
+    submitHandler: function() {
+      $("form").on("submit", function(e) {
+        e.preventDefault();
+        
+        $.ajax({
+          type: "post",
+          url: "../../index.php",
+          data: $(this).serialize(),
+          success: function(data) {
+            if (data == '') {
+              alert("Administrateur enregistré ! Veuillez vous connecter.");
+              location.replace("../../login.html");
+            } else {
+              alert(data);
+            }
+          },
+          error: function() {
+            alert("Erreur. L'administrateur n'a pas été créé.");
+          },
+        });
+      })
+    }
+  })
+});

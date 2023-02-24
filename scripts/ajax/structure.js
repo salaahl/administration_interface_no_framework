775dcbd8b0ca6a8d2ef4b4ce5e31aa3009e5d396
salaahl <67805036+salaahl@users.noreply.github.com>
2023-02-24 13:34:59 +0100

    Update structure.js
    Mise à jour du message d'erreur

--- scripts/ajax/structure.js
+++ scripts/ajax/structure.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-  var pageS = "ok";
   var searchParams = new URLSearchParams(window.location.search);
   var mail = searchParams.get("mail_s");
   var mailP = searchParams.get("mail_p");
@@ -7,10 +6,10 @@ $(document).ready(function () {
   $.ajax({
     type: "POST",
     url: "../index.php",
-    data: { page_structure: pageS, mail: mail },
+    data: { page_structure: 'initialize', mail: mail },
     dataType: "JSON",
     success: function (data) {
-       $("#delete_structure").val(mail);
+      $("#delete_structure").val(mail);
       $("h1").append(data.nom_partenaire);
       $("#mail_structure").append(mail);
       $("#adresse_structure").append(data.adresse);
@@ -18,13 +17,12 @@ $(document).ready(function () {
       $("#perm_boissons").prop("checked", data.perm_boissons);
       $("#perm_newsletter").prop("checked", data.perm_newsletter);
       $("#perm_planning").prop("checked", data.perm_planning);
-      $("#retour_page_partenaire").attr(
-        "href",
+      $("#retour_page_partenaire").attr("href",
         "../front-end/partenaire.php?mail_p=" + data.mail_partenaire
       );
     },
     error: function () {
-      alert("Erreur");
+      alert("L'initialisation du profil a échouée. Veuillez contacter un administrateur.");
     },
   });
 });

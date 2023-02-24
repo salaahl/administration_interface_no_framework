5a3bf41814bc8214bf005ac34f7c8a5d1275bf13
salaahl <67805036+salaahl@users.noreply.github.com>
2023-02-24 15:02:27 +0100

    Update and rename ajouter_structure.php to available_cities.php

--- back-end/CRUD/read/available_cities.php
+++ back-end/CRUD/read/available_cities.php
@@ -1,14 +1,13 @@
 <?php
 
 // Va récupérer les partenaires dispo et les injecter dans la page d'ajout d'une structure
-if (isset($_POST['part_structure'])) {
+if (isset($_POST['available_cities'])) {
     $reponse = [];
     $partenairesQ = $db->query(
         "SELECT (nom)
         FROM FitnessP_Partenaire
         ORDER BY nom"
     );
-
 
     foreach ($partenairesQ as $partenaire) {
         $reponse[] = htmlspecialchars($partenaire['nom']);

97831acbb007a49d5d83a17b6e0e29f8b8a80528
salaahl <67805036+salaahl@users.noreply.github.com>
2023-02-24 13:50:53 +0100

    Update and rename liste_part.php to partners_list.php
    Mise à jour des liens

--- front-end/partners_list.php
+++ front-end/partners_list.php
@@ -17,8 +17,7 @@ if (!isset($_SESSION['admin'])) {
   <meta name="viewport" content="width=device-width">
   <title>Liste des partenaires</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-  <link href="../css/liste_part.css" rel="stylesheet" type="text/css" />
+  <link href="../css/partners_list.css" rel="stylesheet" type="text/css" />
   <link href="../css/sidebar.css" rel="stylesheet" type="text/css" />
 </head>
 
@@ -42,9 +41,17 @@ if (!isset($_SESSION['admin'])) {
       <div id="liste_part"></div>
     </main>
   </div>
+   <script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM="
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.min.js"
+    integrity="sha512-rstIgDs0xPgmG6RX1Aba4KV5cWJbAMcvRCVmglpam9SoHZiUCyQVDdH2LPlxoHtrv17XWblE/V/PP+Tr04hbtA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/additional-methods.min.js"
+    integrity="sha512-6S5LYNn3ZJCIm0f9L6BCerqFlQ4f5MwNKq+EthDXabtaJvg3TuFLhpno9pcm+5Ynm6jdA9xfpQoMz2fcjVMk9g=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
   <script src="../scripts/composants/sidebar.js"></script>
-  <script src="../scripts/ajax/liste_part.js"></script>
-  <script src="../scripts/ajax/filtre_actifs.js"></script>
+  <script src="../scripts/ajax/partners_list.js"></script>
+  <script src="../scripts/ajax/partners_filter.js"></script>
 </body>
 
 </html>

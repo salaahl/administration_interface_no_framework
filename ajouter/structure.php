51621b788011f39aac1b88537461b0d09cea6f6e
salaahl <67805036+salaahl@users.noreply.github.com>
2023-02-24 14:20:33 +0100

    Update structure.php

--- ajouter/structure.php
+++ ajouter/structure.php
@@ -15,7 +15,7 @@ if (!isset($_SESSION['admin'])) {
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width">
   <title>Ajouter une structure</title>
-  <link href="../css/add_franchise.css" rel="stylesheet" type="text/css" />
+  <link href="../css/add_user.css" rel="stylesheet" type="text/css" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 

1605378970f1a71535de9b28ed05610c625c2058
salaahl <67805036+salaahl@users.noreply.github.com>
2023-02-24 13:52:44 +0100

    Update and rename partenaire.php to partner.php
    Mise à jour des liens

--- front-end/partner.php
+++ front-end/partner.php
@@ -19,19 +19,16 @@ if (!isset($_SESSION['admin'])) {
 ?>
 
 <html lang="fr">
-
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width">
   <title>Page partenaire</title>
   <!-- CSS only -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-  <link href="../css/partenaire.css" rel="stylesheet" type="text/css" />
+  <link href="../css/partner.css" rel="stylesheet" type="text/css" />
   <link href="../css/bouton_desac.css" rel="stylesheet" type="text/css" />
   <link href="../css/sidebar.css" rel="stylesheet" type="text/css" />
 </head>
-
 <body>
   <div class="row">
     <div class="col-4 col-md-4 col-lg-3" id="sidebar"></div>
@@ -105,16 +102,24 @@ if (!isset($_SESSION['admin'])) {
   </div>
   <!-- JavaScript Bundle with Popper -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+   <script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM="
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.min.js"
+    integrity="sha512-rstIgDs0xPgmG6RX1Aba4KV5cWJbAMcvRCVmglpam9SoHZiUCyQVDdH2LPlxoHtrv17XWblE/V/PP+Tr04hbtA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/additional-methods.min.js"
+    integrity="sha512-6S5LYNn3ZJCIm0f9L6BCerqFlQ4f5MwNKq+EthDXabtaJvg3TuFLhpno9pcm+5Ynm6jdA9xfpQoMz2fcjVMk9g=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
   
   <?php if (isset($_SESSION['niveau_droits']) && $_SESSION['niveau_droits'] > 2) { ?>
     <script src="../scripts/composants/sidebar.js"></script>
   <?php } else { ?>
     <script src="../scripts/composants/sidebar_part_struc.js"></script>
   <?php } ?>
-
-  <script src="../scripts/ajax/toggle_partenaire.js"></script>
-  <script src="../scripts/ajax/partenaire.js"></script>
-  <script src="../scripts/ajax/statut_part.js"></script>
+  
+  <script src="../scripts/ajax/partner.js"></script>
+  <script src="../scripts/ajax/partner_status.js"></script>
+  <script src="../scripts/ajax/partner_toggle.js"></script>
 </body>
 
 </html>

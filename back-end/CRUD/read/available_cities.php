f043ddce93eae05591e9d4abe66d207e57dd02af
salaahl <67805036+salaahl@users.noreply.github.com>
2023-02-25 10:36:30 +0100

    Update available_cities.php

--- back-end/CRUD/read/available_cities.php
+++ back-end/CRUD/read/available_cities.php
@@ -2,15 +2,12 @@
 
 // Va récupérer les partenaires dispo et les injecter dans la page d'ajout d'une structure
 if (isset($_POST['available_cities'])) {
-    $reponse = [];
     $partenairesQ = $db->query(
         "SELECT (nom)
         FROM FitnessP_Partenaire
         ORDER BY nom"
     );
-
-    foreach ($partenairesQ as $partenaire) {
-        $reponse[] = htmlspecialchars($partenaire['nom']);
-    }
-    echo json_encode($reponse);
+    $response = array_map('htmlspecialchars', $partenairesQ);
+    
+    echo json_encode($response);
 }

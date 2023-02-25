1b6b3ebe12834438272939df22e7a7dde1c7b7d4
salaahl <67805036+salaahl@users.noreply.github.com>
2023-02-25 10:28:40 +0100

    Update user_delete.php

--- back-end/CRUD/delete/user_delete.php
+++ back-end/CRUD/delete/user_delete.php
@@ -1,7 +1,7 @@
 <?php
 
 if (isset($_POST['partner_delete'])) {
-  $mail = mysqli_real_escape_string($db, $_POST['mail_partenaire']);
+  $mail = mysqli_real_escape_string($db, $_POST['partner_mail']);
 
   $partnerD = $db->prepare(
     "DELETE FROM FitnessP_Partenaire
@@ -10,10 +10,18 @@ if (isset($_POST['partner_delete'])) {
 
   $partnerD->bind_param("s", $mail);
   $partnerD->execute();
+  
+  $structureD = $db->prepare(
+    "DELETE FROM FitnessP_Structure
+    WHERE mail = ?"
+  );
+
+  $structureD->bind_param("s", $mail);
+  $structureD->execute();
 }
 
 if (isset($_POST['structure_delete'])) {
-  $mail = mysqli_real_escape_string($db, $_POST['mail_partenaire']);
+  $mail = mysqli_real_escape_string($db, $_POST['structure_mail']);
 
   $structureD = $db->prepare(
     "DELETE FROM FitnessP_Structure

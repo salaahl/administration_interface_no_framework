84eaac9ff60a809530e8d87ea0543eaef796b80c
salaahl <67805036+salaahl@users.noreply.github.com>
2023-02-25 13:03:45 +0100

    Update partner_new.php

--- back-end/CRUD/update/partner_new.php
+++ back-end/CRUD/update/partner_new.php
@@ -2,108 +2,108 @@
 
 // Va gérer la logique des formulaires de création de partenaires
 if (isset($_POST["partner_name"])) {
-  $nom = mysqli_real_escape_string($db, $_POST["partner_name"]);
-  $mail = mysqli_real_escape_string($db, $_POST["mail_partenaire"]);
-  $password = mysqli_real_escape_string($db, $_POST["mot_de_passe_partenaire"]);
+  $city = mysqli_real_escape_string($db, $_POST["partner_name"]);
+  $mail = mysqli_real_escape_string($db, $_POST["partner_mail"]);
+  $password = mysqli_real_escape_string($db, $_POST["partner_password"]);
 
   $passwordHash = password_hash($password, PASSWORD_DEFAULT);
 
-  // Etape 1 : vérifie que le mail n'est pas déjà utilisé par une structure
-  $mail_verif = $db->prepare("SELECT mail
-    FROM FitnessP_Structure
-    WHERE mail = ?");
+  // Je vérifie que le mail est dispo dans mes différents tableaux SQL
+  $check = $db->prepare(
+    "SELECT EXISTS(
+      SELECT FitnessP_Admin.mail_admin, FitnessP_Partenaire.mail, FitnessP_Structure.mail
+      FROM FitnessP_Admin, FitnessP_Partenaire, FitnessP_Structure
+      WHERE 
+      FitnessP_Admin.mail = ?
+      OR
+      FitnessP_Partenaire.mail = ?
+      OR
+      FitnessP_Structure.mail = ?
+    )"
+  );
+  $check->bind_param("sss", $mail, $mail, $mail);
+  $check->execute();
+  $check->store_result();
+  $check->bind_result($exist);
+  $check->fetch();
+  $check->close();
 
-  $mail_verif->bind_param("s", $mail);
-  $mail_verif->execute();
-  $mail_verif->store_result();
-  $mail_verif->bind_result($verif);
-  $mail_verif->fetch();
-  $mail_verif->close();
-
-  // Si le résultat est positif :
-  if ($verif == null) {
-
+  if ($exist == false) {
     $partenaireR = $db->prepare(
-      "INSERT IGNORE INTO FitnessP_Partenaire (nom, mail, mot_de_passe)
+      "INSERT INTO FitnessP_Partenaire (nom, mail, mot_de_passe)
       VALUES (?, ?, ?)"
     );
-
-    // Le triple 's' indique 'string, string, string' pour chacune de mes entrées.
-    // https://www.php.net/manual/fr/mysqli-stmt.bind-param.php
-    $partenaireR->bind_param("sss", $nom, $mail, $passwordHash);
+    $partenaireR->bind_param("sss", $city, $mail, $passwordHash);
     $partenaireR->execute();
-
 
     // Si la requête aboutit... Evite que le mail ne soit envoyé pour rien si la requête échoue
     if (mysqli_affected_rows($db) > 0) {
       $partenaireR->close();
 
       $mailConfirmation =
-        "
-      <html lang='fr'>
-  <head>
-    <meta charset='utf-8'>
-    <meta name='viewport' content='width=device-width'>
-    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx' crossorigin='anonymous'>
-    
-    <style>
-      body
-      {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-      table
-      {
-        width: 50vw;
-        border: 1rem solid highlight;
-        background-color: lightgray;
-      }
-      th
-      {
-        background-color: gray;
-      }
-      td
-      {
-        border: 1px solid gray;
-      }
-    </style>
-    
-  </head>
-  <body>
-    <table>
-      <thead>
-        <tr>
-          <th colspan='2'>Bienvenue sur Fitness P !</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td colspan='2'>Bonjour partenaire de " . htmlspecialchars($nom) . ",<br>Voici vos identifiants de connexion. Notez que le mot de passe n'est valable que pour la première connexion. Il
-            devra être changé lors de la première connexion au site.</td>
-        </tr>
-        <tr>
-          <td>mail :</td>
-          <td>" .
-        htmlspecialchars($mail) .
-        "</td>
-        </tr>
-        <tr>
-          <td>mot de passe :</td>
-          <td>" .
-        htmlspecialchars($password) .
-        "</td>
-        </tr>
-        <tr>
-          <td colspan='2'><a href='https://ecf-salaha.herokuapp.com/login.html' class='btn btn-primary'>Se connecter</a></td>
-        </tr>
-      </tbody>
-    </table>
-  <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js' integrity='sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa' crossorigin='anonymous'></script>
-  </body>
-  </html>";
+        "<html lang='fr'>
+          <head>
+            <meta charset='utf-8'>
+            <meta name='viewport' content='width=device-width'>
+            <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx' crossorigin='anonymous'>
 
-      // Etape 2 : envoyer un mail...
+            <style>
+              body
+              {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+              }
+              table
+              {
+                width: 50vw;
+                border: 1rem solid highlight;
+                background-color: lightgray;
+              }
+              th
+              {
+                background-color: gray;
+              }
+              td
+              {
+                border: 1px solid gray;
+              }
+            </style>
+
+          </head>
+          <body>
+            <table>
+              <thead>
+                <tr>
+                  <th colspan='2'>Bienvenue sur Fitness P !</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td colspan='2'>Bonjour partenaire de " . htmlspecialchars($city) . ",<br>Voici vos identifiants de connexion. Notez que le mot de passe n'est valable que pour la première connexion. Il
+                    devra être changé lors de la première connexion au site.</td>
+                </tr>
+                <tr>
+                  <td>mail :</td>
+                  <td>" .
+                htmlspecialchars($mail) .
+                "</td>
+                </tr>
+                <tr>
+                  <td>mot de passe :</td>
+                  <td>" .
+                htmlspecialchars($password) .
+                "</td>
+                </tr>
+                <tr>
+                  <td colspan='2'><a href='https://ecf-salaha.herokuapp.com/login.html' class='btn btn-primary'>Se connecter</a></td>
+                </tr>
+              </tbody>
+            </table>
+            <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js' integrity='sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa' crossorigin='anonymous'></script>
+          </body>
+         </html>";
+
       // Initialisation des données. Méthode de récup différente selon l'environnemment
       $from_email =
         $_SERVER["SERVER_NAME"] == "localhost"

3d240cdaa6f456ea93ca29492ed65d83213a0b80
salaahl <67805036+salaahl@users.noreply.github.com>
2023-02-25 11:39:08 +0100

    Update structure_page.php

--- back-end/CRUD/read/structure_page.php
+++ back-end/CRUD/read/structure_page.php
@@ -1,14 +1,13 @@
 <?php
 
-
 // PAGE D'UNE STRUCTURE :
 if (isset($_POST['structure_page']) && isset($_POST['mail'])) {
 
   $mail = $_POST['mail'];
-  $reponse = [];
+  $response = [];
 
   $structureQ = $db->prepare(
-    "SELECT *
+    "SELECT mail_part, adresse, mail, perm_boissons, perm_newsletter, perm_boissons
     FROM FitnessP_Structure
     WHERE mail = ?"
   );
@@ -16,21 +15,21 @@ if (isset($_POST['structure_page']) && isset($_POST['mail'])) {
   $structureQ->bind_param("s", $mail);
   $structureQ->execute();
   $structureQ->store_result();
-  $structureQ->bind_result($mailP, $adresse, $mailS, $mdp, $droits, $premiereConnexion, $permB, $permN, $permP);
+  $structureQ->bind_result($partner_mail, $address, $structure_mail, $drinks_permission, $newsletter_permission, $planning_permission);
   $structureQ->fetch();
 
-  $reponse['mail_partenaire'] = htmlspecialchars($mailP);
-  $reponse['adresse'] = htmlspecialchars($adresse);
-  $reponse['mail'] = htmlspecialchars($mailS);
-  $reponse['perm_boissons'] = $permB;
-  $reponse['perm_newsletter'] = $permN;
-  $reponse['perm_planning'] = $permP;
+  $response['partner_mail'] = htmlspecialchars($partner_mail);
+  $response['address'] = htmlspecialchars($address);
+  $response['structure_mail'] = htmlspecialchars($structure_mail);
+  $response['drinks_permission'] = $drinks_permission;
+  $response['newsletter_permission'] = $newsletter_permission;
+  $response['planning_permission'] = $planning_permission;
 
   // Conversion de mon niveau de droits en boolean
   if ($droits == 1) {
-    $reponse['statut'] = true;
+    $reponse['status'] = true;
   } else {
-    $reponse['statut'] = false;
+    $reponse['status'] = false;
   }
 
   $structureQ->close();
@@ -44,10 +43,10 @@ if (isset($_POST['structure_page']) && isset($_POST['mail'])) {
   $PartenaireQ->bind_param("s", $mailP);
   $PartenaireQ->execute();
   $PartenaireQ->store_result();
-  $PartenaireQ->bind_result($nom);
+  $PartenaireQ->bind_result($city);
   $PartenaireQ->fetch();
 
-  $reponse['nom_partenaire'] = htmlspecialchars($nom);
+  $response['city'] = htmlspecialchars($city);
 
-  echo json_encode($reponse);
+  echo json_encode($response);
 }

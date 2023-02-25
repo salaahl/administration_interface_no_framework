2d2ee566c343f564f976f618d87b9cc0ab0a83a2
salaahl <67805036+salaahl@users.noreply.github.com>
2023-02-25 12:46:21 +0100

    Update admin_new.php
    Refactoring du code

--- back-end/CRUD/update/admin_new.php
+++ back-end/CRUD/update/admin_new.php
@@ -2,26 +2,33 @@
 
 // Va gérer la logique des formulaires de création d'admins
 if (isset($_POST['admin_mail']) && isset($_POST['password'])) {
+  
   $mail = mysqli_real_escape_string($db, $_POST['admin_mail']);
   $password = mysqli_real_escape_string($db, $_POST['password']);
-
   $passwordHash = password_hash($password, PASSWORD_DEFAULT);
 
-  // Etape 1 : vérifie que le mail n'est pas déjà utilisé par une structure
-  $mail_verif = $db->prepare("SELECT mail_admin
-    FROM FitnessP_Admin
-    WHERE mail_admin = ?");
+  // Je vérifie que le mail est dispo dans mes différents tableaux SQL
+  $check = $db->prepare(
+    "SELECT EXISTS(
+      SELECT FitnessP_Admin.mail_admin, FitnessP_Partenaire.mail, FitnessP_Structure.mail
+      FROM FitnessP_Admin, FitnessP_Partenaire, FitnessP_Structure
+      WHERE 
+      FitnessP_Admin.mail = ?
+      OR
+      FitnessP_Partenaire.mail = ?
+      OR
+      FitnessP_Structure.mail = ?
+    )"
+  );
 
-  $mail_verif->bind_param("s", $mail);
-  $mail_verif->execute();
-  $mail_verif->store_result();
-  $mail_verif->bind_result($verif);
-  $mail_verif->fetch();
-  $mail_verif->close();
+  $check->bind_param("sss", $mail, $mail, $mail);
+  $check->execute();
+  $check->store_result();
+  $check->bind_result($exist);
+  $check->fetch();
+  $check->close();
 
-  // Etape 2 : Si le résultat est positif...
-  if ($verif == null) {
-
+  if ($exist == false) {
     $adminR = $db->prepare(
       "INSERT INTO FitnessP_Admin (mail_admin, mot_de_passe_admin)
     VALUES (?, ?)"
@@ -29,14 +36,5 @@ if (isset($_POST['admin_mail']) && isset($_POST['password'])) {
 
     $adminR->bind_param("ss", $mail, $passwordHash);
     $adminR->execute();
-
-    if (mysqli_affected_rows($db) > 0) {
-      $adminR->close();
-    } else {
-      echo "Erreur. Le mail est déjà pris.";
-    }
-  } else {
     $adminR->close();
-    echo "Erreur. Le mail est déjà pris.";
-  }
 }

243e04e34488f4c9f089c673b7c9d7c1a46b8231
salaahl <67805036+salaahl@users.noreply.github.com>
2023-02-28 14:21:05 +0100

    Update partner_toggle.js

--- scripts/ajax/partner_toggle.js
+++ scripts/ajax/partner_toggle.js
@@ -1,23 +1,19 @@
-// Se déclenchera chaque fois que N'IMPORTE QUEL switch de ma page sera modifié :
 $(".toggle").change(function () {
-
-  let toggle = $(this).prop("checked");
-  let id = $(this).attr("id");
-  let nom = document.getElementById("nom_partenaire").textContent.slice(13);
-  let permission = id.slice(5);
+  let partnerToggle = $(this).prop("checked");
+  let toggleName = $(this).attr("name");
+  let partnerCity = document.getElementById("nom_partenaire").textContent.slice(13);
   let searchParams = new URLSearchParams(window.location.search);
-  let mail = searchParams.get("mail_p");
+  let partnerMail = searchParams.get("partner_mail");
 
   if (confirm("Veuillez confirmer votre choix") == true) {
     $.ajax({
       url: "../index.php",
       method: "POST",
       data: {
-        toggle_partenaire: toggle,
-        id: id,
-        mail: mail,
-        nom: nom,
-        permission: permission,
+        partner_toggle: partnerToggle,
+        toggle_name: toggleName,
+        partner_mail: partnerMail,
+        partner_city: partnerCity
       },
       error: function () {
         alert("Impossible de mettre à jour la permission. Veuillez contacter un administrateur.");

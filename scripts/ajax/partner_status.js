b114ae84db6d999406854f10d6fb46da4bc8057b
salaahl <67805036+salaahl@users.noreply.github.com>
2023-02-28 15:53:13 +0100

    Update partner_status.js

--- scripts/ajax/partner_status.js
+++ scripts/ajax/partner_status.js
@@ -1,13 +1,13 @@
 $("#partner_status").change(function () {
   var toggle = $(this).prop("checked");
   var searchParams = new URLSearchParams(window.location.search);
-  var mail = searchParams.get("mail_p");
+  var partnerMail = searchParams.get("partner_mail");
 
   if (confirm("Veuillez confirmer votre action") == true) {
     $.ajax({
       url: "../index.php",
       method: "POST",
-      data: { partner_activate: toggle, mail: mail },
+      data: { partner_activate: toggle, partner_mail: partnerMail },
       error: function () {
         alert('Impossible de mettre à jour le statut. Veuillez contacter un administrateur.')
       },

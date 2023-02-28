406b0a4542ad02e089c92f9b8824b32aa657331f
salaahl <67805036+salaahl@users.noreply.github.com>
2023-02-28 13:06:15 +0100

    Update and rename partner.php to partner_page.php

--- front-end/partner_page.php
+++ front-end/partner_page.php
@@ -1,6 +1,5 @@
 <?php session_start();
 
-
 // Niveau 2 : partenaire
 // 1è vérification : attribution d'un passe-droit pour l'admin
 if (!isset($_SESSION['admin'])) {
@@ -81,23 +80,22 @@ if (!isset($_SESSION['admin'])) {
         <h6>Permissions globales : </h6>
         <!-- Switch 1 -->
         <div class="form-check form-switch">
-          <input class="form-check-input toggle" type="checkbox" role="switch" id="drinks_permission" />
+          <input type="checkbox" role="switch" class="form-check-input toggle" id="drinks_permission" name="drinks_permission" />
           <label class="form-check-label" for="drinks_permission">Vente de boissons</label>
         </div>
         <!-- Switch 2 -->
         <div class="form-check form-switch">
-          <input class="form-check-input toggle" type="checkbox" role="switch" id="newsletter_permission" />
+          <input type="checkbox" role="switch" class="form-check-input toggle" id="newsletter_permission" name="newsletter_permission" />
           <label class="form-check-label" for="newsletter_permission">Envoyer une newsletter</label>
         </div>
         <!-- Switch 3 -->
         <div class="form-check form-switch">
-          <input class="form-check-input toggle" type="checkbox" role="switch" id="planning_permission" />
+          <input type="checkbox" role="switch" class="form-check-input toggle" id="planning_permission" name="planning_permission" />
           <label class="form-check-label" for="planning_permission">Gérer le planning d'une équipe</label>
         </div>
       </div>
     </div>
   </div>
-  <!-- JavaScript Bundle with Popper -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
    <script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM="
     crossorigin="anonymous"></script>
@@ -108,7 +106,7 @@ if (!isset($_SESSION['admin'])) {
     integrity="sha512-6S5LYNn3ZJCIm0f9L6BCerqFlQ4f5MwNKq+EthDXabtaJvg3TuFLhpno9pcm+5Ynm6jdA9xfpQoMz2fcjVMk9g=="
     crossorigin="anonymous" referrerpolicy="no-referrer"></script>
   
-  <?php if (isset($_SESSION['niveau_droits']) && $_SESSION['niveau_droits'] > 2) { ?>
+  <?php if (isset($_SESSION['rights']) && $_SESSION['rights'] > 2) { ?>
     <script src="../scripts/composants/sidebar.js"></script>
   <?php } else { ?>
     <script src="../scripts/composants/sidebar_part_struc.js"></script>

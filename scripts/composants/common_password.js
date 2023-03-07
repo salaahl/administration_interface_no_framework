214f935c70c21f726b90be2c35d6cb78ba9067d1
salaahl <67805036+salaahl@users.noreply.github.com>
2023-03-07 17:41:20 +0100

    Update common_password.js

--- scripts/composants/common_password.js
+++ scripts/composants/common_password.js
@@ -1,6 +1,6 @@
 function commonPassword (input) 
 {
-    NBP.init("mostcommon_10000", "collections", true);
+    NBP.init("mostcommon_10000", "/app/collections", true);
     let password = $(input).val();
 
     if (NBP.isCommonPassword(password))

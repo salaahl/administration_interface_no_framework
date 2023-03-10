d4108904df34ddb7a627312542afed90a34fd174
salaahl <67805036+salaahl@users.noreply.github.com>
2023-03-10 17:07:17 +0100

    Update structure_page.php

--- front-end/structure_page.php
+++ front-end/structure_page.php
@@ -29,10 +29,12 @@ if (!isset($_SESSION['admin'])) {
 
 <!DOCTYPE html>
 <html lang="fr">
-
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width">
+  <meta charset="utf-8" />
+  <meta name= "author" lang="fr" content= "Salaha SOKHONA" >
+  <meta name="copyright" content="Salaha SOKHONA." />
+  <meta name="description" content="Interface d'administration à destination des franchisés de la marque Fitness P."/>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Page structure</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
   <link href="../css/structure_page.css" rel="stylesheet" type="text/css" />

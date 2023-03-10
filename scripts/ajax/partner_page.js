f5f5db2aeb9c163dadc7ce4f044689e13ed8ee94
salaahl <67805036+salaahl@users.noreply.github.com>
2023-03-10 13:47:46 +0100

    Update partner_page.js

--- scripts/ajax/partner_page.js
+++ scripts/ajax/partner_page.js
@@ -8,28 +8,6 @@ $(document).ready(function () {
     data: { partner_page: "initialize", city: city },
     dataType: "JSON",
     success: function (data) {
-      if (data != "") {
-        for (let c = 0; data.structures_mails.length > c; c++) {
-          $("#structures").append(
-            '<div class="structure">' +
-              '<a id="structure_' +
-              c +
-              '" href="structure_page.php?structure_mail=' +
-              data.structures_mails[c] +
-              "&city=" +
-              city +
-              '">' +
-              '<div class="structure_address">' +
-              data.addresses[c] +
-              "</div>" +
-              '<div class="structure_mail">' +
-              data.structures_mails[c] +
-              "</div>" +
-              "</a>" +
-              "</div>"
-          );
-        }
-      }
       $("#city h1").append(city);
       $("#mail").append(data.mail);
       $("#delete-user").val(data.mail);
@@ -37,6 +15,23 @@ $(document).ready(function () {
       $("#drinks-permission").prop("checked", data.drinks_permission);
       $("#newsletter-permission").prop("checked", data.newsletter_permission);
       $("#planning-permission").prop("checked", data.planning_permission);
+      
+      for (let c = 0; data.structures_mails.length > c; c++) {
+        $("#structures").append(
+          '<div class="structure">' +
+            '<a href="structure_page.php?structure_mail=' +
+              data.structures_mails[c] +
+              "&city=" + city + '">' +
+            '<div class="structure-address">' +
+              data.addresses[c] +
+            "</div>" +
+            '<div class="structure-mail">' +
+              data.structures_mails[c] +
+            '</div>' +
+            '</a>' +
+          '</div>'
+        );
+      }
     },
     error: function () {
       alert(

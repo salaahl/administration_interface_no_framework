bb259baf532b2facb1bebd3c1719306875723718
salaahl <67805036+salaahl@users.noreply.github.com>
2023-03-10 17:05:06 +0100

    Update partner_search.php

--- front-end/partner_search.php
+++ front-end/partner_search.php
@@ -11,8 +11,11 @@ if (!isset($_SESSION['admin'])) {
 <!DOCTYPE html>
 <html lang="fr">
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width">
+    <meta charset="utf-8" />
+    <meta name= "author" lang="fr" content= "Salaha SOKHONA" >
+    <meta name="copyright" content="Salaha SOKHONA." />
+    <meta name="description" content="Interface d'administration à destination des franchisés de la marque Fitness P."/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Recherche d'un partenaire</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link href="../css/partner_search.css" rel="stylesheet" type="text/css" />

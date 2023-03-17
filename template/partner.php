146ffb87e1b43fbf0021f3d585f9a9a21555b015
salaahl <67805036+salaahl@users.noreply.github.com>
2023-03-17 12:05:44 +0100

    Update partner.php

--- template/partner.php
+++ template/partner.php
@@ -32,12 +32,12 @@ if (!isset($_SESSION['admin'])) {
 </head>
 
 <body>
-  <aside>
+  <nav>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#sidebar" aria-controls="sidebar" aria-expanded="false" aria-label="Toggle navigation">
       <span class="menu-text">MENU</span><span class="navbar-toggler-icon"></span>
     </button>
     <div id="sidebar" class="collapse"></div>
-  </aside>
+  </nav>
   <main>
     <div class="en-tete">
       <div id="city">
